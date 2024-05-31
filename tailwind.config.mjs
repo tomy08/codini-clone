d7f50a2a750b7d96285aba73c56fb5fe66a0ebdc
tomy08 <tomasesantacruz@gmail.com>
2024-05-31 15:22:10 -0300

    💄 chore: Update header component and add footer component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'blue-primary': '#031CFF',
+        'gray-secondary': '#242E3A',
+        'orange-section': '#F05927',
+        'pink-section': '#F75E8D',
+        'yellow-section': '#FFD000',
+        'blue-section': '#25AEBC',
+        'green-section': '#05D785 ',
+      },
+    },
+  },
+  plugins: [],
 }
